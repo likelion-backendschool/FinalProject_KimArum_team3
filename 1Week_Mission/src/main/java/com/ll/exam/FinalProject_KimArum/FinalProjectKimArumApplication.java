e884431f3868942b1c7078076878d0373b492b71
Aruming <0430kar@naver.com>
2022-10-17 19:54:46 +0900

    [#1-feat]프로젝트 세팅

---
+++ 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/FinalProjectKimArumApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.FinalProject_KimArum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectKimArumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectKimArumApplication.class, args);
+	}
+
+}
---
+++ 1Week_Mission/src/test/java/com/ll/exam/FinalProject_KimArum/FinalProjectKimArumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.FinalProject_KimArum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectKimArumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
