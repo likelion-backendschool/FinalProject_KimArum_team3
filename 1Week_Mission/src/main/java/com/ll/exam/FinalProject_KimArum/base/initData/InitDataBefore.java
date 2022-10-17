2b0edd31ef703a33d4aedbbd9884b93499937165
Aruming <0430kar@naver.com>
2022-10-17 22:02:03 +0900

    [#1-feat]닉네임 입력란 추가, 테스트데이터 닉네임 null 처리

--- 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/base/initData/InitDataBefore.java
+++ 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/base/initData/InitDataBefore.java
@@ -6,6 +6,6 @@ import com.ll.exam.FinalProject_KimArum.app.member.service.MemberService;
 public interface InitDataBefore {
     default void before(MemberService memberService) {
         Member member1 = memberService.join("user1", "1234", "user1@test.com", "hamster1");
-        Member member2 = memberService.join("user2", "1234", "user2@test.com", "");
+        Member member2 = memberService.join("user2", "1234", "user2@test.com", null);
     }
 }
