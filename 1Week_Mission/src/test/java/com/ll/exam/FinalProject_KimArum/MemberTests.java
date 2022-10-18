4e0023982372a2914dbccc7935fbf73bc77d9294
Aruming <0430kar@naver.com>
2022-10-18 10:33:57 +0900

    [#1-test] 회원 정보 수정 테스트케이스 추가

--- 1Week_Mission/src/test/java/com/ll/exam/FinalProject_KimArum/MemberTests.java
+++ 1Week_Mission/src/test/java/com/ll/exam/FinalProject_KimArum/MemberTests.java
@@ -7,13 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -93,5 +94,48 @@ public class MemberTests {
                 .andExpect(redirectedUrlPattern("/member/login?msg=**"));
 
         assertThat(memberService.findByUsername("user999").isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("회원정보 수정 폼")
+    @WithUserDetails("user1")
+    void t4() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/member/modify"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showModify"))
+                .andExpect(content().string(containsString("회원 정보 수정")));
+    }
+
+    @Test
+    @DisplayName("회원정보수정 후 프로필페이지 이동, 회원정보 보여야 함")
+    @WithUserDetails("user1")
+    void t5() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/member/modify")
+                        .with(csrf())
+                        .param("username", "user1")
+                        .param("email", "user1@test2.com")
+                        .param("nickname", "햄스터")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is3xxRedirection())
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("modify"))
+                .andExpect(redirectedUrl("/member/profile"))
+                .andExpect(content().string("user1@test2.com"))
+                .andExpect(content().string("햄스터"));
+
+        assertThat(memberService.findByUsername("user1").isPresent()).isTrue();
     }
 }
