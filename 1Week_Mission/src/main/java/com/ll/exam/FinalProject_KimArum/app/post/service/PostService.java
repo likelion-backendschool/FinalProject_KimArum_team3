1cdc3bc370c135af67638d1397e92eea0015271c
Aruming <0430kar@naver.com>
2022-10-18 20:33:21 +0900

    [#1-feat] 글 내용 html rendering 결,과 저장

--- 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/post/service/PostService.java
+++ 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/post/service/PostService.java
@@ -4,6 +4,9 @@ import com.ll.exam.FinalProject_KimArum.app.member.entity.Member;
 import com.ll.exam.FinalProject_KimArum.app.post.entity.Post;
 import com.ll.exam.FinalProject_KimArum.app.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,11 +17,12 @@ import java.util.Optional;
 public class PostService {
     private final PostRepository postRepository;
 
-    public Post writePost(Long authorId, String subject, String content) {
+    public Post writePost(Long authorId, String subject, String content, String htmlContent) {
         Post post = Post.builder()
                 .author(new Member(authorId))
                 .subject(subject)
                 .content(content)
+                .contentHtml(htmlContent)
                 .build();
 
         postRepository.save(post);
@@ -29,5 +33,12 @@ public class PostService {
     @Transactional(readOnly = true)
     public Optional<Post> findBySubject(String subject) {
         return postRepository.findBySubject(subject);
+    }
+
+    public String markdownToHtml(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
     }
 }
