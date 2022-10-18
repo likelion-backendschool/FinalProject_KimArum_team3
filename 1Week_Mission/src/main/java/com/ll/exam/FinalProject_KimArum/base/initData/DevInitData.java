20e08dcab61c0f79ccace76772121017813cfef7
Aruming <0430kar@naver.com>
2022-10-19 00:19:16 +0900

    [#1-test] 글 테스트데이터 추가

--- 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/base/initData/DevInitData.java
+++ 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/base/initData/DevInitData.java
@@ -1,6 +1,7 @@
 package com.ll.exam.FinalProject_KimArum.base.initData;
 
 import com.ll.exam.FinalProject_KimArum.app.member.service.MemberService;
+import com.ll.exam.FinalProject_KimArum.app.post.service.PostService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +11,9 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevInitData implements InitDataBefore {
     @Bean
-    CommandLineRunner initData(MemberService memberService) {
+    CommandLineRunner initData(MemberService memberService, PostService postService) {
         return args -> {
-            before(memberService);
+            before(memberService, postService);
         };
     }
 }
