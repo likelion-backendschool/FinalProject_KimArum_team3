b91b5b96c5050e950178d8c490c957f2038ef8fe
Aruming <0430kar@naver.com>
2022-10-18 22:40:50 +0900

    [#1-feat] 글 리스트 구현, 메인페이지를 글 리스트로 변경

--- 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/post/repository/PostRepository.java
+++ 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/post/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.ll.exam.FinalProject_KimArum.app.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findPostBySubject(String subject);
 
     Optional<Post> findPostById(Long id);
+
+    List<Post> findAllByOrderByIdDesc();
 }
