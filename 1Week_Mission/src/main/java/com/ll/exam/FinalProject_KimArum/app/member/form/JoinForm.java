af409d147ff46eeed3d20d1c97684e62474642a0
Aruming <0430kar@naver.com>
2022-10-21 11:12:56 +0900

    [#3-refact] 가입 시 예외처리 controller에서 처리, bindingResult 사용

--- 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/member/form/JoinForm.java
+++ 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/member/form/JoinForm.java
@@ -3,13 +3,17 @@ package com.ll.exam.FinalProject_KimArum.app.member.form;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 public class JoinForm {
     @NotEmpty
+    @Size(min = 1, max = 10, message = "아이디는 1자 이상, 10자 이하로 설정 가능합니다.")
     private String username;
     @NotEmpty
     private String password;
+    @NotEmpty
+    private String passwordConfirm;
     @NotEmpty
     private String email;
 
