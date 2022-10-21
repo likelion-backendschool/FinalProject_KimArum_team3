c3bc2a340e472475fd00486160fcf65f9dd2060f
Aruming <0430kar@naver.com>
2022-10-21 11:53:09 +0900

    [#3-refact] BindingResult를 사용한 예외 처리 추가

--- 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/post/form/PostForm.java
+++ 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/post/form/PostForm.java
@@ -3,12 +3,15 @@ package com.ll.exam.FinalProject_KimArum.app.post.form;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 public class PostForm {
     @NotEmpty
+    @Size(min=1, max = 20, message = "제목을 20자 이하로 입력해주세요.")
     private String subject;
     @NotEmpty
+    @Size(min=1, max = 2000, message = "내용을 2000자 이하로 입력해주세요.")
     private String content;
     private String hashTagContents;
 }
