ba727c4941f3fa88f001a84397f5199405e174de
Aruming <0430kar@naver.com>
2022-10-24 10:32:37 +0900

    [#3-feat] 상품 등록, 상품 상세 구현

--- 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/product/controller/ProductController.java
+++ 1Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/product/controller/ProductController.java
@@ -1,22 +1,34 @@
 package com.ll.exam.FinalProject_KimArum.app.product.controller;
 
+import com.ll.exam.FinalProject_KimArum.app.member.entity.Member;
+import com.ll.exam.FinalProject_KimArum.app.post.entity.Post;
 import com.ll.exam.FinalProject_KimArum.app.post.entity.PostKeyword;
 import com.ll.exam.FinalProject_KimArum.app.post.service.PostKeywordService;
+import com.ll.exam.FinalProject_KimArum.app.product.entity.Product;
+import com.ll.exam.FinalProject_KimArum.app.product.form.ProductForm;
+import com.ll.exam.FinalProject_KimArum.app.product.service.ProductService;
 import com.ll.exam.FinalProject_KimArum.app.secutiry.dto.MemberContext;
+import com.ll.exam.FinalProject_KimArum.base.Rq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/product")
 public class ProductController {
+    private final ProductService productService;
     private final PostKeywordService postKeywordService;
+    private final Rq rq;
 
     @GetMapping("/create")
     public String showCreate(@AuthenticationPrincipal MemberContext memberContext, Model model) {
@@ -26,5 +38,24 @@ public class ProductController {
         model.addAttribute("keywords", keywords);
 
         return "product/create";
+    }
+
+    @PreAuthorize("isAuthenticated() and hasAuthority('ADMIN')")
+    @PostMapping("/create")
+    public String create(@Valid ProductForm productForm){
+        Member member = rq.getMember();
+        Product product = productService.create(member, productForm.getSubject(), productForm.getPrice(), productForm.getPostKeywordId(), productForm.getProductTagContents());
+        return "redirect:/product/" + product.getId();
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        Product product = productService.findForPrintById(id).get();
+        List<Post> posts = productService.findPostsByProduct(product);
+
+        model.addAttribute("product", product);
+        model.addAttribute("posts", posts);
+
+        return "product/detail";
     }
 }
