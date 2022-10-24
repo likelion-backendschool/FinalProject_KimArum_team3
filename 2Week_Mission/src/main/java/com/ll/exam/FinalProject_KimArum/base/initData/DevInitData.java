320e9b4a519e4350420afbc606176467a586312c
Aruming <0430kar@naver.com>
2022-10-24 17:22:29 +0900

    [#6-feat] 장바구니에 상품 추가, 삭제 구현 / 테스트데이터 추가

--- 2Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/base/initData/DevInitData.java
+++ 2Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/base/initData/DevInitData.java
@@ -1,5 +1,6 @@
 package com.ll.exam.FinalProject_KimArum.base.initData;
 
+import com.ll.exam.FinalProject_KimArum.app.cart.service.CartService;
 import com.ll.exam.FinalProject_KimArum.app.member.service.MemberService;
 import com.ll.exam.FinalProject_KimArum.app.post.service.PostService;
 import com.ll.exam.FinalProject_KimArum.app.product.service.ProductService;
@@ -12,9 +13,9 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevInitData implements InitDataBefore {
     @Bean
-    CommandLineRunner initData(MemberService memberService, PostService postService, ProductService productService) {
+    CommandLineRunner initData(MemberService memberService, PostService postService, ProductService productService, CartService cartService) {
         return args -> {
-            before(memberService, postService, productService);
+            before(memberService, postService, productService, cartService);
         };
     }
 }
