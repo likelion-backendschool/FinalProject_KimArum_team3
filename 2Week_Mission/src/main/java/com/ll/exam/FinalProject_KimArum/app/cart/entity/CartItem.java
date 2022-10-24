8fee2307d68116f13e95f55b4a897f2f6ed4f612
Aruming <0430kar@naver.com>
2022-10-24 17:22:29 +0900

    [#6-feat] 장바구니에 상품 추가, 삭제 구현 / 테스트데이터 추가

---
+++ 2Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/cart/entity/CartItem.java
@@ -0,0 +1,28 @@
+package com.ll.exam.FinalProject_KimArum.app.cart.entity;
+
+import com.ll.exam.FinalProject_KimArum.app.member.entity.Member;
+import com.ll.exam.FinalProject_KimArum.app.product.entity.Product;
+import com.ll.exam.FinalProject_KimArum.base.entity.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class CartItem extends BaseEntity {
+    @ManyToOne(fetch = LAZY)
+    private Member buyer;
+    @ManyToOne(fetch = LAZY)
+    private Product product;
+}
--- 2Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/base/initData/InitDataBefore.java
+++ 2Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/base/initData/InitDataBefore.java
@@ -1,5 +1,7 @@
 package com.ll.exam.FinalProject_KimArum.base.initData;
 
+import com.ll.exam.FinalProject_KimArum.app.cart.entity.CartItem;
+import com.ll.exam.FinalProject_KimArum.app.cart.service.CartService;
 import com.ll.exam.FinalProject_KimArum.app.member.entity.Member;
 import com.ll.exam.FinalProject_KimArum.app.member.service.MemberService;
 import com.ll.exam.FinalProject_KimArum.app.post.entity.Post;
@@ -8,7 +10,7 @@ import com.ll.exam.FinalProject_KimArum.app.product.entity.Product;
 import com.ll.exam.FinalProject_KimArum.app.product.service.ProductService;
 
 public interface InitDataBefore {
-    default void before(MemberService memberService, PostService postService, ProductService productService) {
+    default void before(MemberService memberService, PostService postService, ProductService productService, CartService cartService) {
         Member member1 = memberService.join("user1", "1234", "user1@test.com");
         Member member2 = memberService.join("user2", "1234", "user2@test.com");
 
@@ -17,8 +19,15 @@ public interface InitDataBefore {
 
         Post post1 = postService.writePost(member1.getId(), "1번 글", "1번 내용", "1번 내용", "#태그1 #태그2");
         Post post2 = postService.writePost(member1.getId(), "2번 글", "2번 내용", "2번 내용", "#태그2 #태그3");
+        Post post3 = postService.writePost(member1.getId(), "3번 글", "3번 내용", "3번 내용", "#태그2 #태그3 #태그4");
+        Post post4 = postService.writePost(member1.getId(), "4번 글", "4번 내용", "4번 내용", "#태그4 #태그5");
 
         Product product1 = productService.create(member1, "1번 상품", 1200, 1, "#상품태그1 #상품태그2");
         Product product2 = productService.create(member1, "2번 상품", 2400, 2, "#상품태그2 #상품태그3");
+        Product product3 = productService.create(member1, "3번 상품", 3600, 3, "#상품태그1 #상품태그2");
+        Product product4 = productService.create(member1, "4번 상품", 4800, 4, "#상품태그2 #상품태그3");
+
+        CartItem cartItem1 = cartService.addItem(member2, product1);
+        CartItem cartItem2 = cartService.addItem(member2, product2);
     }
 }
