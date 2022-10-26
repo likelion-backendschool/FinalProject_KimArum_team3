78fae4c078246f1d5d2c2d14276ebfe220489a88
Aruming <0430kar@naver.com>
2022-10-26 17:05:08 +0900

    [#6-feat] 결제 완료 도서, 구매자 정보 MyBook에 저장, 환불 도서는 삭제

--- 2Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/mybook/service/MyBookService.java
+++ 2Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/mybook/service/MyBookService.java
@@ -1,6 +1,7 @@
 package com.ll.exam.FinalProject_KimArum.app.mybook.service;
 
 import com.ll.exam.FinalProject_KimArum.app.member.entity.Member;
+import com.ll.exam.FinalProject_KimArum.app.mybook.entity.MyBook;
 import com.ll.exam.FinalProject_KimArum.app.mybook.repository.MyBookRepository;
 import com.ll.exam.FinalProject_KimArum.app.product.entity.Product;
 import com.ll.exam.FinalProject_KimArum.app.product.service.ProductService;
@@ -17,5 +18,36 @@ public class MyBookService {
 
     public void addMyBook(Member buyer, Long orderId) {
         List<Product> books = productService.findProductByOrderId(orderId);
+
+        for(Product book : books){
+            saveMyBook(buyer, book.getId());
+        }
+    }
+
+    private MyBook saveMyBook(Member buyer, long bookId) {
+        Product product = productService.getProductById(bookId);
+        MyBook myBook = MyBook.builder()
+                .buyer(buyer)
+                .product(product)
+                .build();
+
+        myBookRepository.save(myBook);
+
+        return myBook;
+    }
+
+
+    public void refundedMyBook(Member buyer, Long orderId) {
+        List<Product> books = productService.findProductByOrderId(orderId);
+
+        for(Product book : books){
+            deleteMyBook(buyer, book.getId());
+        }
+    }
+
+    private void deleteMyBook(Member buyer, long bookId){
+        MyBook myBook = myBookRepository.findByBuyerIdAndProductId(buyer.getId(), bookId);
+
+        myBookRepository.delete(myBook);
     }
 }
