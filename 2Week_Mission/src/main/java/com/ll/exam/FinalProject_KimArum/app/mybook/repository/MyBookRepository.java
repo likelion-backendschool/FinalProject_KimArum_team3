9168608afa9873c36477e581e876088c389f67bb
Aruming <0430kar@naver.com>
2022-10-26 15:52:52 +0900

    [#6-feat] myBook 관련 추가

---
+++ 2Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/mybook/repository/MyBookRepository.java
@@ -0,0 +1,7 @@
+package com.ll.exam.FinalProject_KimArum.app.mybook.repository;
+
+import com.ll.exam.FinalProject_KimArum.app.mybook.entity.MyBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MyBookRepository extends JpaRepository<MyBook, Long> {
+}
