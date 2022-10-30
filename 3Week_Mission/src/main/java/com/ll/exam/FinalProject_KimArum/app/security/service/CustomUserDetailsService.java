d6afab0c0a5dfb6d458a39b2a0e5011444f5fba3
Aruming <0430kar@naver.com>
2022-10-30 21:35:02 +0900

    [#10-feat] user2 회원을 관리자로 지정

--- 3Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/security/service/CustomUserDetailsService.java
+++ 3Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/security/service/CustomUserDetailsService.java
@@ -4,10 +4,15 @@ import com.ll.exam.FinalProject_KimArum.app.member.entity.Member;
 import com.ll.exam.FinalProject_KimArum.app.member.repository.MemberRepository;
 import com.ll.exam.FinalProject_KimArum.app.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +22,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member member = memberRepository.findByUsername(username).get();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        if (member.getUsername().equals("user2")) {
+            authorities.add(new SimpleGrantedAuthority("ADMIN"));
+        }
 
         return new MemberContext(member, member.genAuthorities());
     }
