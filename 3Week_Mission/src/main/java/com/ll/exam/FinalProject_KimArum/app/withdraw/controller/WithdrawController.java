64473b903638c00f36cccf697fcc039297baee4a
Aruming <0430kar@naver.com>
2022-11-02 15:23:06 +0900

    [#10-refact] 출금 신청 금액 범위 설정

--- 3Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/withdraw/controller/WithdrawController.java
+++ 3Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/withdraw/controller/WithdrawController.java
@@ -1,7 +1,13 @@
 package com.ll.exam.FinalProject_KimArum.app.withdraw.controller;
 
+import com.ll.exam.FinalProject_KimArum.app.base.rq.Rq;
+import com.ll.exam.FinalProject_KimArum.app.member.entity.Member;
+import com.ll.exam.FinalProject_KimArum.app.member.service.MemberService;
+import com.ll.exam.FinalProject_KimArum.app.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,10 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/withdraw")
 public class WithdrawController {
+    private final MemberService memberService;
+    private final Rq rq;
 
     @GetMapping
     @RequestMapping("/apply")
-    public String showApplyWithdraw(){
+    public String showApplyWithdraw(Model model){
+        Member member = rq.getMember();
+
+        long restCash = memberService.getRestCash(member);
+        System.out.println(restCash);
+
+        model.addAttribute("restCash", restCash);
+
         return "withdraw/apply";
     }
 }
