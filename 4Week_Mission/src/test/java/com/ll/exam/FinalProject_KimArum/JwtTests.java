9a211839a333c70fc6df74add60a37550bb48092
Aruming <0430kar@naver.com>
2022-11-07 11:46:10 +0900

    [#15-setting] 테스트 폴더 생성

---
+++ 4Week_Mission/src/test/java/com/ll/exam/FinalProject_KimArum/JwtTests.java
@@ -0,0 +1,9 @@
+package com.ll.exam.FinalProject_KimArum;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class JwtTests {
+}
