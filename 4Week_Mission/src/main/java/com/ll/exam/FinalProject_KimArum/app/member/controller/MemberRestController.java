a4e46409cb75dfca889f691b8096870793a7dc56
Aruming <0430kar@naver.com>
2022-11-08 11:44:41 +0900

    [#15-feat] MemberRestController에 RsData 도입, 응답 본문에 jwt키 포함되도록

--- 4Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/member/controller/MemberRestController.java
+++ 4Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/member/controller/MemberRestController.java
@@ -1,11 +1,11 @@
 package com.ll.exam.FinalProject_KimArum.app.member.controller;
 
+import com.ll.exam.FinalProject_KimArum.app.base.dto.RsData;
 import com.ll.exam.FinalProject_KimArum.app.member.entity.Member;
 import com.ll.exam.FinalProject_KimArum.app.member.form.LoginForm;
 import com.ll.exam.FinalProject_KimArum.app.member.service.MemberService;
+import com.ll.exam.FinalProject_KimArum.util.Ut;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,26 +21,32 @@ public class MemberRestController {
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginForm loginForm) {
+    public ResponseEntity<RsData> login(@RequestBody LoginForm loginForm) {
         if (loginForm.isNotValid()) {
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            return Ut.spring.responseEntityOf(RsData.of("F-1", "로그인 정보가 올바르지 않습니다."));
         }
 
         Member member = memberService.findByUsername(loginForm.getUsername()).orElse(null);
 
-        if(member == null){
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        if (member == null) {
+            return Ut.spring.responseEntityOf(RsData.of("F-2", "일치하는 회원이 존재하지 않습니다."));
         }
 
         if (passwordEncoder.matches(loginForm.getPassword(), member.getPassword()) == false) {
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            return Ut.spring.responseEntityOf(RsData.of("F-3", "비밀번호가 일치하지 않습니다."));
         }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authentication", "JWT키");
+        String accessToken = "JWT_Access_Token";
 
-        String body = "username : %s, password : %s".formatted(loginForm.getUsername(), loginForm.getPassword());
-
-        return new ResponseEntity<>(body, headers, HttpStatus.OK);
+        return Ut.spring.responseEntityOf(
+                RsData.of(
+                        "S-1",
+                        "로그인 성공, Access Token을 발급합니다.",
+                        Ut.mapOf(
+                                "accessToken", accessToken
+                        )
+                ),
+                Ut.spring.httpHeadersOf("Authentication", accessToken)
+        );
     }
 }
