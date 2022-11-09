b28403cd561f3c6dbf8f00d15d7947baa84bd0b4
Aruming <0430kar@naver.com>
2022-11-09 15:57:48 +0900

    [#15-refact] /member/me 응답 본문을 요구사항과 일치시킴

---
+++ 4Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/member/form/MemberDto.java
@@ -0,0 +1,39 @@
+package com.ll.exam.FinalProject_KimArum.app.member.form;
+
+import com.ll.exam.FinalProject_KimArum.app.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberDto {
+
+    Long id;
+    LocalDateTime createDate;
+    LocalDateTime modifyDate;
+    String username;
+    String email;
+    boolean emailVerified;
+    String nickname;
+
+    public static MemberDto getApiMemberByMember(Member member) {
+
+        MemberDto memberDto = MemberDto.builder()
+                .id(member.getId())
+                .createDate(member.getCreateDate())
+                .modifyDate(member.getModifyDate())
+                .username(member.getUsername())
+                .email(member.getEmail())
+                .emailVerified(true)
+                .nickname(member.getNickname())
+                .build();
+
+        return memberDto;
+    }
+}
