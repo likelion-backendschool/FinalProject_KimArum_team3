0076fbfeb2c009af170323978d31a2cd82178ed0
Aruming <0430kar@naver.com>
2022-11-09 16:25:44 +0900

    [#15-refact] SecurityConfig 개선

--- 4Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/security/SecurityConfig.java
+++ 4Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/security/SecurityConfig.java
@@ -1,57 +1,44 @@
 package com.ll.exam.FinalProject_KimArum.app.security;
 
 
-import com.ll.exam.FinalProject_KimArum.app.jwt.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
-@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+    private final AuthenticationFailureHandler authenticationFailureHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .httpBasic().disable()
+                .formLogin(
+                        formLogin -> formLogin
+                                .loginPage("/member/login") // GET
+                                .loginProcessingUrl("/member/login") // POST
+                                .successHandler(authenticationSuccessHandler)
+                                .failureHandler(authenticationFailureHandler)
+                )
                 .authorizeRequests(
                         authorizeRequests -> authorizeRequests
-                                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                                .antMatchers("/api/v1/member/login", "member/join", "member/login")
-                                .permitAll()
+                                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+                                .hasAuthority("ADMIN")
                                 .anyRequest()
-                                .authenticated() // 최소자격 : 로그인
+                                .permitAll()
                 )
-                .cors().disable() // 타 도메인에서 API 호출 가능
-                .csrf().disable() // CSRF 토큰 끄기
-                .httpBasic().disable() // httpBaic 로그인 방식 끄기
-                .formLogin().disable() // 폼 로그인 방식 끄기
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(STATELESS)
-                ).addFilterBefore(
-                        jwtAuthorizationFilter,
-                        UsernamePasswordAuthenticationFilter.class
+                .logout(
+                        logout -> logout
+                                .logoutUrl("/member/logout")
                 );
-//                .formLogin(
-//                        formLogin -> formLogin
-//                                .loginPage("/member/login") // GET
-//                                .loginProcessingUrl("/member/login") // POST
-//                                .successHandler(authenticationSuccessHandler)
-//                                .failureHandler(authenticationFailureHandler)
-//                )
-//                .logout(
-//                        logout -> logout
-//                                .logoutUrl("/member/logout")
-//                );
 
         return http.build();
     }
