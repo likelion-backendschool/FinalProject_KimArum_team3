43f89d6c17da8794adc9c20dd73984fdca95876d
Aruming <0430kar@naver.com>
2022-11-10 10:34:06 +0900

    [#18-chore] 함수명 변경, 패키지 이동

--- 4Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/member/dto/MemberDto.java
+++ 4Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/app/member/dto/MemberDto.java
@@ -1,4 +1,4 @@
-package com.ll.exam.FinalProject_KimArum.app.member.form;
+package com.ll.exam.FinalProject_KimArum.app.member.dto;
 
 import com.ll.exam.FinalProject_KimArum.app.member.entity.Member;
 import lombok.AllArgsConstructor;
@@ -22,7 +22,7 @@ public class MemberDto {
     boolean emailVerified;
     String nickname;
 
-    public static MemberDto getApiMemberByMember(Member member) {
+    public static MemberDto memberToMemberDto(Member member) {
 
         MemberDto memberDto = MemberDto.builder()
                 .id(member.getId())
