2fe35faef87f891d337603bd5fd089c0efd1020b
Aruming <0430kar@naver.com>
2022-11-12 13:38:23 +0900

    [#18-feat] Member 엔티티에 accessToken 추가

--- 4Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/util/Ut.java
+++ 4Week_Mission/src/main/java/com/ll/exam/FinalProject_KimArum/util/Ut.java
@@ -18,6 +18,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -28,6 +29,9 @@ public class Ut {
     }
 
     public static class date {
+        public static LocalDateTime bitsToLocalDateTime(List<Integer> bits) {
+            return LocalDateTime.of(bits.get(0), bits.get(1), bits.get(2), bits.get(3), bits.get(4), bits.get(5), bits.get(6));
+        }
         public static int getEndDayOf(int year, int month) {
             String yearMonth = year + "-" + "%02d".formatted(month);
 
